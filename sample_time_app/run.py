c1801db97e50f11da9003f5d4356af278a58cb50
Qing Mu <lynx@Qings-MacBook-Pro-2.local>
2025-03-10 19:33:30 -0400

    Initial commit: Added modified run.py and Flask app

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import datetime
 app = Flask(__name__)
 
 
@@ -6,7 +7,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello world!'
 
+@app.route('/time')
+def get_time():
+    return {"current_time": datetime.datetime.now().isoformat()}
 
 app.run(host='0.0.0.0',
         port=8080,
         debug=True)
+
+
+
